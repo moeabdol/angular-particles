3a85cea3a15af3d6d941207a45df809be0e30cb0
moeabdol <mohd.a.saed@gmail.com>
2017-10-15 18:53:07 +0300

    Implement basic space particles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 
+declare var particlesJS: any;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  constructor() {
+    particlesJS.load('particles', 'assets/particles.json', null);
+  }
 }
